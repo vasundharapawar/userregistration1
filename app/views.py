34b1589259fa0dc404c027acd4c2ab089371dc76
vasundharapawar <pawarvasundhara199@gmail.com>
2024-01-20 19:05:59 +0530

    first commit

--- app/views.py
+++ app/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render
 from app.forms import *
+from django.http import HttpResponse
 
 # Create your views here.
 
@@ -7,4 +8,21 @@ def registration(request):
     ufo=Userform()
     pfo=Profileform()
     d={'ufo':ufo,'pfo':pfo}
-    return render(request,'registration.html',d)
+
+    if request.method=='POST' and request.FILES:
+        ufd=Userform(request.POST)
+        pfd=Profileform(request.POST,request.FILES)
+
+        if ufd.is_valid() and pfd.is_valid():
+            MUFDO=ufd.save(commit=False)
+            pw=ufd.cleaned_data['password']
+            MUFDO.set_password(pw)
+            MUFDO.save()
+
+            MPFDO=pfd.save(commit=False)
+            MPFDO.username=MUFDO
+            MPFDO.save()
+            return HttpResponse('Registration is done successfully')
+        #else:
+           # return HttpResponse('Invalid data')
+    return render(request,'registration.html',d)    
--- userregistration1/settings.py
+++ userregistration1/settings.py
@@ -120,7 +120,10 @@ USE_TZ = True
 
 STATIC_URL = 'static/'
 
-# Default primary key field type
+MEDIA_URL='media/'
+MEDIA_ROOT=os.path.join(BASE_DIR,'media')
+
+# Default primary key field type 
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
